b8651414aa99f780317bbe591be2afc9c5f0d72d
Marceltxk <marceltxk@gmail.com>
2025-04-14 03:06:50 -0300

    add Cliente.java

---
+++ src/Cliente.java
@@ -0,0 +1,17 @@
+public class Cliente {
+    private String nome, telefone, email;
+    
+    public Cliente(String nome, String telefone, String email) {
+        this.nome = nome;
+        this.telefone = telefone;
+        this.email = email;
+    }
+
+    public String getNome() { return nome; }
+    public String getTelefone() { return telefone; }
+    public String getEmail() { return email };
+
+    public void setNome(String nome)  { this.nome = nome; }
+    public void setTelefone(String tel) { this.telefone = tel; }
+    public void setEmail(String email) { this.email = email; }
+}
